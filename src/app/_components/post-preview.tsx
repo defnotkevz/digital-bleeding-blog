f4a336143093d0273321fd7ef63aaf5de7dea638
kevinsourcode <kevnroy1@gmail.com>
2024-05-24 22:02:41 +0530

    fix align issues with date

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -14,12 +14,12 @@ type Props = {
 export function PostPreview({ title, date, excerpt, slug }: Props) {
   return (
     <div className="w-full mb-52 flex flex-col justify-center items-center mt-36">
-      <h3 className="text-5xl max-w-2xl md:text-7xl lg:text-7xl font-bold tracking-tighter leading-tight md:leading-none mb-6 md:text-left">
+      <h3 className="text-5xl max-w-2xl md:text-7xl lg:text-7xl font-bold tracking-tighter leading-tight md:leading-none w-full mb-6 md:text-left">
         <Link href={`/posts/${slug}`} className="hover:underline ">
           {title}
         </Link>
       </h3>
-      <div className="text-lg mb-8 italic w-[25rem] md:w-[42rem] lg:w-[42rem] text-left">
+      <div className="text-lg mb-8 italic w-full md:w-[42rem] lg:w-[42rem] text-left">
         <DateFormatter dateString={date} />
       </div>
       <p className="text-xl leading-relaxed mb-4 max-w-2xl">{excerpt}</p>
